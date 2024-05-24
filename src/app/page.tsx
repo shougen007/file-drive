dd74ccf42daa8a9c3103ff8203f6129c0b75cb72
sanghyun <son@sanghyuns-Mac-mini.local>
2024-05-24 21:55:22 +0900

    * (Button) Create a client component for the Convex provider *

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@
 
 import { Button } from "@/components/ui/button";
 import { SignInButton, SignOutButton, SignedIn, SignedOut } from "@clerk/nextjs";
-import { useMutation } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
 
-  //const session = useSession(); 
-  const createFile = useMutation(api.file.createFile);
+  const files = useQuery(api.files.getFiles);
+  const createFile = useMutation(api.files.createFile);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -22,7 +22,9 @@ export default function Home() {
         <Button>Sign In</Button>
         </SignInButton>
       </SignedOut>
-      
+
+      {files?.map((file) => <div key={file._id}>{file.name}</div>)}
+     
       <Button onClick={() => {
         createFile({
           name: "hello world",
